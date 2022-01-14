ff9d46255b1bf5cbe92bb4848de0bcc868b8e165
Konstantin Balakirev <kbalakirev@gmail.com>
2022-01-14 14:50:12 -0600

    pwd in other file

--- bash.js
+++ bash.js
@@ -1,9 +1,13 @@
 // Output a prompt
 process.stdout.write("prompt > ");
 
-process.stdin.on("data", (data) => {
-  const cmd = data.toString().trim(); // remove the newline
+// process.stdin.on("data", (data) => {
+//   const cmd = data.toString().trim(); // remove the newline
 
-  process.stdout.write("You typed: " + cmd);
-  process.stdout.write(`\nprompt > `);
-});
+//   process.stdout.write("You typed: " + cmd);
+//   process.stdout.write(`\nprompt > `);
+// });
+
+
+const pwd3 = require('./pwd2');
+pwd3();
---
+++ pwd2.js
@@ -0,0 +1,11 @@
+function pwd (){
+    process.stdin.on('data', (data) => {
+        const cmd = data.toString().trim();
+        if(cmd === 'pwd'){
+        process.stdout.write(process.cwd());
+        process.stdout.write(`\nprompt > `);
+    }
+  });
+}
+
+module.exports = pwd;
